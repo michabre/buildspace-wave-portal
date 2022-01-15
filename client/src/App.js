8036236032e256fa2be6794521dc3da0a58b02cc
Michael <michaelcbreuer@gmail.com>
2022-01-15 22:37:43 +0800

    adding footer and cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import * as React from "react";
-import { ColorModeScript, useColorMode, Box, Button, Center, Container, VStack, Heading, Text } from '@chakra-ui/react';
-import { ethers } from "ethers";
+import * as React from "react"
+import { 
+  ColorModeScript, 
+  useColorMode, 
+  Box, 
+  Button, 
+  Container, 
+  Flex, 
+  VStack, 
+  Heading, 
+  Text } from '@chakra-ui/react'
+import { ethers } from "ethers"
 import theme from './theme'
-import Header from "./components/layout/Header";
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
 
-import './App.css';
+import './App.css'
 
 export default function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode()
 
   const wave = () => {
     console.log('wave')
@@ -17,25 +27,28 @@ export default function App() {
     <>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Header mode={toggleColorMode} current={colorMode} />
-      <Container maxW='container.md' centerContent>
-        <Box padding='4'>
-          <VStack>
-        
-            <Heading as='h2'>👋 Hey there!</Heading>
+      <Container maxW='container.md' h='60vh'>
+        <Flex h='100%' align='center'>
+          <Box>
+            <VStack h='100%'>
+          
+              <Heading as='h2'>👋 Hey there!</Heading>
 
-            <Box padding='4'>
-              <Text fontSize='lg' align='center'>
-                My name is Michael and this my first Buildspace project. Connect your Ethereum wallet and wave at me!
-              </Text>
-            </Box>
+              <Box padding='4'>
+                <Text fontSize='lg' align='center'>
+                  My name is Michael and this my first Buildspace project. Connect your Ethereum wallet and wave at me!
+                </Text>
+              </Box>
 
-            <Button onClick={wave}>
-              Wave at Me
-            </Button>
+              <Button onClick={wave}>
+                Wave at Me
+              </Button>
 
-          </VStack>
-        </Box>
+            </VStack>
+          </Box>
+        </Flex>
       </Container>
+      <Footer />
     </>
-  );
+  )
 }
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+
+const Footer = () => { 
+  return (
+    <Flex h='30vh'>
+      <Box w='100%' p={4}>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Footer
