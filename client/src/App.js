c6cdeda4ee4a435ea429f5bf2b5a2f391195080d
Michael <michaelcbreuer@gmail.com>
2022-01-15 22:13:16 +0800

    housekeeping

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { ColorModeScript, useColorMode } from '@chakra-ui/react';
-import { Container } from '@chakra-ui/react';
+import { ColorModeScript, useColorMode, Box, Button, Center, Container, VStack, Heading, Text } from '@chakra-ui/react';
 import { ethers } from "ethers";
 import theme from './theme'
 import Header from "./components/layout/Header";
@@ -11,28 +10,32 @@ export default function App() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   const wave = () => {
-    
+    console.log('wave')
   }
   
   return (
     <>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <Header mode={toggleColorMode} current={colorMode} />
-    <Container maxW='container.xl'>
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Header mode={toggleColorMode} current={colorMode} />
+      <Container maxW='container.md' centerContent>
+        <Box padding='4'>
+          <VStack>
+        
+            <Heading as='h2'>👋 Hey there!</Heading>
 
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
+            <Box padding='4'>
+              <Text fontSize='lg' align='center'>
+                My name is Michael and this my first Buildspace project. Connect your Ethereum wallet and wave at me!
+              </Text>
+            </Box>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-      </div>
-    </Container>
+            <Button onClick={wave}>
+              Wave at Me
+            </Button>
+
+          </VStack>
+        </Box>
+      </Container>
     </>
   );
 }
